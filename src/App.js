dd1647ef571c3a799b1e394f46a1d3e724c5c026
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-02-26 01:31:14 +0100

    +under construction flag

--- src/App.js
+++ src/App.js
@@ -8,25 +8,26 @@ const App = ({ logout }) => {
         <Col>
           <div
             style={{ minHeight: "100vh" }}
-            className="d-flex justify-content-center align-items-center"
+            className="d-flex flex-column justify-content-center align-items-center"
           >
-            <p className="display-1">weatherapp</p>
-            <p>
-              <div className="position-absolute top-0 end-0 p-0 p-sm-2">
-                <ButtonGroup>
-                  <Button
-                    href="https://github.com/nikolav/palmhr-gddsxoybumg.git"
-                    target="_blank"
-                  >
-                    <i class="fa-brands fa-github"></i>
-                  </Button>
-                  <Button onClick={logout} variant="secondary" id="_3">
-                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                  </Button>
-                </ButtonGroup>
-              </div>
-            </p>
+            <h1 className="display-1">weatherapp</h1>
+            <p className="text-muted">under construction</p>
           </div>
+          <p>
+            <div className="position-absolute top-0 end-0 p-0 p-sm-2">
+              <ButtonGroup>
+                <Button
+                  href="https://github.com/nikolav/palmhr-gddsxoybumg.git"
+                  target="_blank"
+                >
+                  <i class="fa-brands fa-github"></i>
+                </Button>
+                <Button onClick={logout} variant="secondary" id="_3">
+                  <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                </Button>
+              </ButtonGroup>
+            </div>
+          </p>
         </Col>
       </Row>
     </Container>
