98658b330cd8e24455934413f7926426798d9755
nikolav <admin@nikolav.rs>
2022-02-26 04:19:26 +0000

    --amend

--- src/components/weather-widget/WeatherWidget.jsx
+++ src/components/weather-widget/WeatherWidget.jsx
@@ -16,23 +16,23 @@ const WeatherWidget = () => {
         <Card.Text className="fs-1">⛅</Card.Text>
         <Card.Text>Some clouds</Card.Text>
         <div style={{ fontSize: "90%" }}>
-          <Card.Text className="d-flex align-items-center justify-content-between">
+          <Card.Text className="my-2 d-flex align-items-center justify-content-between">
             <span>Min</span>
             <span>&deg;1</span>
           </Card.Text>
-          <Card.Text className="d-flex align-items-center justify-content-between">
+          <Card.Text className="my-2 d-flex align-items-center justify-content-between">
             <span>Max</span>
             <span>&deg;2</span>
           </Card.Text>
-          <Card.Text className="d-flex align-items-center justify-content-between">
+          <Card.Text className="my-2 d-flex align-items-center justify-content-between">
             <span>Sunset</span>
             <span>&deg;6:12pm</span>
           </Card.Text>
-          <Card.Text className="d-flex align-items-center justify-content-between">
+          <Card.Text className="my-2 d-flex align-items-center justify-content-between">
             <span>Sunrise</span>
             <span>&deg;7:12am</span>
           </Card.Text>
-          <Card.Text className="d-flex align-items-center justify-content-between">
+          <Card.Text className="my-2 d-flex align-items-center justify-content-between">
             <span>Wind</span>
             <span>&deg;0.45ms</span>
           </Card.Text>
