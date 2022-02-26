2de26c778fba8e5c389c12215fa333cf9af35b48
nikolav <admin@nikolav.rs>
2022-02-26 04:43:26 +0000

    +drag-handle

--- src/components/weather-widget/WeatherWidgetControls.jsx
+++ src/components/weather-widget/WeatherWidgetControls.jsx
@@ -1,11 +1,15 @@
+import WeatherWidgetControlsDragHandles from "./WeatherWidgetControlsDragHandles";
 import WeatherWidgetControlsFilter from "./WeatherWidgetControlsFilter";
 import WeatherWidgetControlsRemove from "./WeatherWidgetControlsRemove";
 
 const WeatherWidgetControls = () => {
   return (
-    <div className="d-flex gap-3 align-items-center justify-content-end">
-      <WeatherWidgetControlsFilter />
-      <WeatherWidgetControlsRemove />
+    <div className="d-flex align-items-center justify-content-between">
+      <WeatherWidgetControlsDragHandles />
+      <div className="d-flex gap-3">
+        <WeatherWidgetControlsFilter />
+        <WeatherWidgetControlsRemove />
+      </div>
     </div>
   );
 };
---
+++ src/components/weather-widget/WeatherWidgetControlsDragHandles.jsx
@@ -0,0 +1,10 @@
+const WeatherWidgetControlsDragHandles = () => {
+  const handleClick = (evt) => {};
+  return (
+    <a href="#" role="button">
+      <i onClick={handleClick} className="text-secondary opacity-25 fa-solid fa-grip-lines"></i>
+    </a>
+  );
+};
+
+export default WeatherWidgetControlsDragHandles;
