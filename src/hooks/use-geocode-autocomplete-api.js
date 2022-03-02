b738110996eee2b728d73a59c0516b4d56b11774
nikolav <admin@nikolav.rs>
2022-03-02 22:07:14 +0000

    ~ports

--- src/hooks/use-geocode-autocomplete-api.js
+++ src/hooks/use-geocode-autocomplete-api.js
@@ -2,8 +2,9 @@ import useHttpGet from "./use-http-get";
 
 const useGeocodeAutocompleteAPI = (INPUT = "") =>
   useHttpGet({
-    url: `http://localhost:3031/api/geocode_autocomplete/${encodeURIComponent(INPUT)}`,
-    // url: `/api/geocode_autocomplete/${encodeURIComponent(INPUT)}`,
+    // local port @3031
+    // url: `http://localhost:3031/api/geocode_autocomplete/${encodeURIComponent(INPUT)}`,
+    url: `/api/geocode_autocomplete/${encodeURIComponent(INPUT)}`,
   });
 
 export default useGeocodeAutocompleteAPI;
