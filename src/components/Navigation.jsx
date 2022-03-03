c59c1b80c010095550c91bf06b00b627a1d05100
nikolav <admin@nikolav.rs>
2022-03-03 01:05:57 +0000

    ~tweeks logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,19 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 
 const Navigation = ({ logout }) => {
   return (
     <>
-      <Navbar bg="--light" expand="sm" variant="dark" className="mb-5 bg-custom-01 border-0">
+      <Navbar
+        bg="--light"
+        expand="sm"
+        variant="dark"
+        className="mb-5 bg-custom-01 border-0"
+      >
         <Container fluid="sm">
-          <Navbar.Brand href="/">Weather Web App</Navbar.Brand>
+          <Link to="/" className="text-decoration-none">
+            <Navbar.Brand className="fs-3 opacity-50">Weather Web App</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import Root from "./Root";
 // import Dashboard from "./Dashboard";
 
 import "./index.css"; // +normalize
-import "./theme/bootstrap.css"; // +bs5 +bootswatch +some-overrides
+import "./theme/bootstrap.css"; // +bs5 +bootswatch +overrides
 
-import "animate.css"; // +simple ccs amimations
+import "animate.css";
 
 
 ReactDOM.render(
