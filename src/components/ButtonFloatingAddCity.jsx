95a2a3ba998f53cbd485e3409c17c4104503c68e
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-03-04 23:00:49 +0100

    ~readme, lists commands

--- src/components/ButtonFloatingAddCity.jsx
+++ src/components/ButtonFloatingAddCity.jsx
@@ -124,12 +124,14 @@ const ButtonFloatingAddCity = ({ addPlace }) => {
                 <motion.div
                   initial={{
                     opacity: 0,
+                    x: 12,
                   }}
                   animate={{
                     opacity: 1,
+                    x: 0,
                   }}
                   transition={{
-                    duration: 0.18,
+                    duration: 0.22,
                   }}
                 >
                   <ListGroup
